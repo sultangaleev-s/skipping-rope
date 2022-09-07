ccea28a2f37c92e0902c72d70ea70fc6eea4993c
Sergey Sultangaleev <sultangaleev.s@yandex.ru>
2022-09-07 16:38:29 +0300

    fix path img

--- src/js/modules/removeProgress.js
+++ src/js/modules/removeProgress.js
@@ -4,12 +4,16 @@ const removeProgress = () => {
     const remove = () => {
         if (confirm('Вы точно хотите стереть результат?')) {
             removeBtn.classList.add('settings__remove_active')
+            if (localStorage.colorBlue) {
+                localStorage.clear()
+                localStorage.colorBlue = 'true'
+            } else {
+                localStorage.clear()
+            }
             
-            localStorage.clear()
             localStorage.minutesProgress = '0'
             localStorage.caloriesProgress = '0'
             localStorage.workoutsProgress = '0'
-
             changeProgress()
 
             setTimeout(() => removeBtn.classList.remove('settings__remove_active'), 3000)
